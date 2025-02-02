46b5360e82171441ff911014ad569eb7ca0b098c
lisandra <obtuosa@obtuosa.bbrouter>
2025-02-02 16:55:46 -0300

    Index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,27 @@
-export default function Page() {
-    return <p>Minha rota!</p>;
+import Link from 'next/link';
+import { getAllPosts } from "../services/api";
+
+export default function Page({posts}) {
+    return(
+      <>
+      <h1>Página Inicial</h1>
+      <p>Blog com objetivo de trazer insights e ideias da minha jornada em TI</p>
+      {
+        posts.map(post =>
+          <p>
+            <Link href={`/${post.slug}`}>
+            {post.title } 
+            </Link>
+          </p>
+        )
+      }
+      </>
+    )
+  }
+
+  export function getStaticProps(){
+    const posts = getAllPosts(['title', 'date', 'slug']);
+    return {
+      props: {posts}
+    }
   }
